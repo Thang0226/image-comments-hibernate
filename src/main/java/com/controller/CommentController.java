82fa5c57d3e2f8dd4bd749192dc7c90d828cf225
Thang0226 <thang.nd0226@gmail.com>
2024-12-21 21:48:48 +0700

    can like comments & show like counts

--- src/main/java/com/controller/CommentController.java
+++ src/main/java/com/controller/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -27,6 +28,16 @@ public class CommentController {
     public String addComment(Comment comment, RedirectAttributes redirectAttributes) {
         commentService.add(comment);
         redirectAttributes.addFlashAttribute("message", "New comment added");
+        return "redirect:/img-of-the-day";
+    }
+
+    @GetMapping("/{id}/like")
+    public String likeComment(@PathVariable int id, RedirectAttributes redirectAttributes) {
+        Comment comment = commentService.findById(id);
+        int currentCount = comment.getLikeCount();
+        comment.setLikeCount(currentCount + 1);
+        commentService.update(comment);
+        redirectAttributes.addFlashAttribute("message", "Liked!");
         return "redirect:/img-of-the-day";
     }
 }
