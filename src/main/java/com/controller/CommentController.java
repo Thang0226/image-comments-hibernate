2aa245c2c8cb559592aaa8a7f08cf8f6831e253e
Thang0226 <thang.nd0226@gmail.com>
2024-12-21 21:29:31 +0700

    can add new comment & show all comments

--- src/main/java/com/controller/CommentController.java
+++ src/main/java/com/controller/CommentController.java
@@ -1,12 +1,14 @@
 package com.controller;
 
-import com.service.CommentService;
+import com.model.Comment;
 import com.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/img-of-the-day")
@@ -19,5 +21,12 @@ public class CommentController {
     public String showComments(Model model) {
         model.addAttribute("comments", commentService.findAll());
         return "index";
+    }
+
+    @PostMapping("/add-comment")
+    public String addComment(Comment comment, RedirectAttributes redirectAttributes) {
+        commentService.add(comment);
+        redirectAttributes.addFlashAttribute("message", "New comment added");
+        return "redirect:/img-of-the-day";
     }
 }
